e7675caf94d435285e4dc441a8f504de99950556
Artur <a.nurtdinow@yandex.ru>
2021-12-13 20:07:18 +0300

    Wall, post, search, profile business logic

--- app/src/main/java/ru/spbstu/blog/root/presentation/RootActivity.kt
+++ app/src/main/java/ru/spbstu/blog/root/presentation/RootActivity.kt
@@ -4,12 +4,16 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 import ru.spbstu.blog.R
 import ru.spbstu.blog.databinding.ActivityMainBinding
 import ru.spbstu.blog.navgiation.Navigator
 import ru.spbstu.blog.root.di.RootApi
 import ru.spbstu.blog.root.di.RootComponent
 import ru.spbstu.common.di.FeatureUtils
+import ru.spbstu.common.events.AuthEvent
 import javax.inject.Inject
 
 class RootActivity : AppCompatActivity() {
@@ -25,6 +29,8 @@ class RootActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        EventBus.getDefault().register(this)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         inject()
@@ -38,6 +44,7 @@ class RootActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         navigator.detach()
+        EventBus.getDefault().unregister(this)
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -49,5 +56,10 @@ class RootActivity : AppCompatActivity() {
             .rootActivityComponentFactory()
             .create(this)
             .inject(this)
+    }
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    fun onEvent(event: AuthEvent) {
+        navigator.goToLogin()
     }
 }
